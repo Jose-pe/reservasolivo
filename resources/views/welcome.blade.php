179270440b13a26a6ae990c8df513ee8a1784867
Jose-pe <joseluis.von@gmail.com>
2026-09-22 23:44:33 -0500

    libro reclamaciones link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -349,7 +349,7 @@
         </div>
         <div class="col-12 col-md-6 d-flex flex-column align-items-center align-self-md-center align-items-md-end">
           <ul class="list-unstyled mb-0">
-            <li><a href="{{route('libro-reclamaciones.create')}}"> <i class="fa-solid fa-plus me-2"></i>Libro de Reclamaciones</li></a>
+           <a class="link-dark" href="{{route('libro-reclamaciones.create')}}"> <li> <i class="fa-solid fa-plus me-2"></i>Libro de Reclamaciones</li></a>
           </ul>
         </div>
     </div>
